b37431433af6cbab33ef5019d56e41e6eb40c622
Lucas Walter <wsacul@gmail.com>
2023-05-29 16:45:00 -0700

    draw bounding box around roi open3d point cloud

--- image_manip_demo/scripts/open3d_viz.py
+++ image_manip_demo/scripts/open3d_viz.py
@@ -17,27 +17,41 @@ from sensor_msgs.msg import (
 )
 
 
-def depth_color_to_pcd(depth_np: np.ndarray, color_np: np.ndarray, camera_info: CameraInfo, roi_x=0, roi_y=0):
+def camera_to_intrinsic(camera_info: CameraInfo) -> o3d.camera.PinholeCameraIntrinsic:
     K = camera_info.K
     fx = K[0]
     fy = K[4]
     cx = K[2]
     cy = K[5]
 
-    width = depth_np.shape[1]
+    width = camera_info.width
+    height = camera_info.height
+    intrinsic = o3d.camera.PinholeCameraIntrinsic(width, height, fx, fy, cx, cy)
+    return intrinsic
+
+
+def adjust_intrinsic_roi(intrinsic: o3d.camera.PinholeCameraIntrinsic, width: int, height: int, roi_x=0, roi_y=0):
+    (fx, fy) = intrinsic.get_focal_length()
+    (cx, cy) = intrinsic.get_principal_point()
+    roi_intrinsic = o3d.camera.PinholeCameraIntrinsic(width, height, fx, fy, cx - roi_x, cy - roi_y)
+    return roi_intrinsic
+
+
+def depth_color_to_pcd(depth_np: np.ndarray, color_np: np.ndarray, camera_info: CameraInfo, roi_x=0, roi_y=0):
     height = depth_np.shape[0]
-    roi_cx = cx - roi_x
-    roi_cy = cy - roi_y
-    intrinsic_o3d = o3d.camera.PinholeCameraIntrinsic(width, height, fx, fy, roi_cx, roi_cy)
+    width = depth_np.shape[1]
+    if width == 0 or height == 0:
+        return None
+    intrinsic = camera_to_intrinsic(camera_info)
+    intrinsic = adjust_intrinsic_roi(intrinsic, width, height, roi_x, roi_y)
     depth_o3d = o3d.geometry.Image(depth_np)
     color_o3d = o3d.geometry.Image(color_np)
     rgbd_o3d = o3d.geometry.RGBDImage.create_from_color_and_depth(color_o3d, depth_o3d,
                                                                   convert_rgb_to_intensity=False)
-    text = f"focal: {intrinsic_o3d.get_focal_length()}, principal {intrinsic_o3d.get_principal_point()}"
+    text = f"focal: {intrinsic.get_focal_length()}, principal {intrinsic.get_principal_point()}"
     text += f" {depth_np.shape} {roi_x} {roi_y}, {rgbd_o3d}"
     rospy.logdebug_throttle(0.0, text)
-    rospy.loginfo(rgbd_o3d)
-    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_o3d, intrinsic_o3d)  # , depth_scale=1.0)
+    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_o3d, intrinsic)  # , depth_scale=1.0)
     return pcd
 
 
@@ -46,13 +60,13 @@ class Open3DViz:
         self.lock = Lock()
 
         self.viz = None
-        self.old_pcd = None
-        self.new_pcd = None
+        self.old_geometry = None
+        self.new_geometry = None
 
         self.cv_bridge = CvBridge()
 
         # TODO(lucasw) use dynamic reconfigure or at least rosparams
-        self.roi_x = 200  # 1000
+        self.roi_x = 200
         self.roi_w = 300
         self.roi_y = 100
         self.roi_h = 500
@@ -92,23 +106,28 @@ class Open3DViz:
             # self.view_control.change_field_of_view(10)
             # rospy.loginfo(self.view_control.get_field_of_view())
             self.viz.get_render_option().point_size = 1.5
+            self.viz.get_render_option().background_color = np.asarray([0.5, 0.5, 0.5])
 
         with self.lock:
-            new_pcd = self.new_pcd
-            self.new_pcd = None
+            new_geometry = self.new_geometry
+            self.new_geometry = None
 
-        if new_pcd is not None:
+        if new_geometry is not None:
             view_param = None
-            if self.old_pcd is not None:
+            if self.old_geometry is not None:
                 # https://github.com/isl-org/Open3D/issues/4306
                 # https://github.com/isl-org/Open3D/issues/2264#issuecomment-1410327023
                 view_param = self.view_control.convert_to_pinhole_camera_parameters()
 
                 # don't remove unless there are new pcds to replace these
-                for pcd in self.old_pcd:
+                for pcd in self.old_geometry:
+                    if pcd is None:
+                        continue
                     self.viz.remove_geometry(pcd)
 
-            for ind, pcd in enumerate(new_pcd):
+            for ind, pcd in enumerate(new_geometry):
+                if pcd is None:
+                    continue
                 # if ind > 0:
                 #     pcd.paint_uniform_color([0.5, 0.206, 0.8])
                 # else:
@@ -125,7 +144,7 @@ class Open3DViz:
                 self.view_control.change_field_of_view(-2.0)
                 rospy.loginfo(self.view_control.get_field_of_view())
 
-            self.old_pcd = new_pcd
+            self.old_geometry = new_geometry
 
         self.viz.poll_events()
         self.viz.update_renderer()
@@ -145,11 +164,16 @@ class Open3DViz:
         t0 = rospy.Time.now()
         depth_np = self.cv_bridge.imgmsg_to_cv2(depth_image, depth_image.encoding)
 
+        new_geometry = []
+
+        width = depth_image.width
+        height = depth_image.height
+
         x0 = self.roi_x
         y0 = self.roi_y
 
         if self.roi_w is None:
-            x1 = depth_image.width
+            x1 = width
         else:
             x1 = x0 + self.roi_w
 
@@ -160,20 +184,30 @@ class Open3DViz:
 
         roi_depth_np = depth_np[y0:y1, x0:x1].copy()
         roi_color_np = color_np[y0:y1, x0:x1, :].copy()
-        print(f"{depth_np.shape}, {color_np.shape}, {roi_depth_np.shape} {roi_color_np.shape}")
+        # rospy.loginfo(f"{depth_np.shape}, {color_np.shape}, {roi_depth_np.shape} {roi_color_np.shape}")
         roi_pcd = depth_color_to_pcd(roi_depth_np, roi_color_np, camera_info, roi_x=x0, roi_y=y0)
+        if roi_pcd is not None:
+            new_geometry.append(roi_pcd)
+            new_geometry.append(roi_pcd.get_axis_aligned_bounding_box())
 
         depth_np[y0:y1, x0:x1] = np.nan
 
         pcd = depth_color_to_pcd(depth_np, color_np, camera_info)
+        if pcd is not None:
+            new_geometry.append(pcd)
+
+        intrinsic = camera_to_intrinsic(camera_info)
+        extrinsic = np.zeros((4, 4))
+        camera_lines = o3d.geometry.LineSet.create_camera_visualization(intrinsic, extrinsic)
+        new_geometry.append(camera_lines)
 
         # rospy.loginfo_throttle(2.0, f"{x0} {x1}, {y0} {y1}")
 
         t1 = rospy.Time.now()
-        rospy.loginfo_throttle(2.0, f"{pcd} {roi_pcd} open3d processing time {(t1 - t0).to_sec()}s")
+        rospy.loginfo_throttle(4.0, f"{pcd} {roi_pcd} open3d processing time {(t1 - t0).to_sec()}s")
         # o3d.visualization.draw_geometries([pcd])
         with self.lock:
-            self.new_pcd = [pcd, roi_pcd]
+            self.new_geometry = new_geometry
 
 
 if __name__ == "__main__":
