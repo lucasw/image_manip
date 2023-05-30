bc2b7078c3e508e3acc6e2df8032386a7aab12c7
Lucas Walter <wsacul@gmail.com>
2023-05-30 06:53:00 -0700

    support any combination of compressed and uncompressed color and depth images for open3d viewer, though haven't tested all combinations

--- image_manip_demo/scripts/open3d_viz.py
+++ image_manip_demo/scripts/open3d_viz.py
@@ -5,6 +5,8 @@
 
 from threading import Lock
 
+import cv2
+import struct
 import message_filters
 import numpy as np
 import open3d as o3d
@@ -15,6 +17,48 @@ from sensor_msgs.msg import (
     CompressedImage,
     Image,
 )
+
+
+# TODO(lucaw) put in importable src file
+# https://answers.ros.org/question/249775/display-compresseddepth-image-python-cv2/
+def decode_compressed_depth(msg: CompressedImage):
+    # 'msg' as type CompressedImage
+    depth_format, compression = msg.format.split(';')
+    # remove white space
+    depth_format = depth_format.strip()
+    compression = compression.strip()
+    compression = compression.split(' ')[0]
+    if compression != "compressedDepth":
+        text = f"Compression type '{compression}' is not 'compressedDepth' '{msg.format}'"
+        raise Exception(text)
+
+    # remove header from raw data
+    depth_header_size = 12
+    raw_header = msg.data[:depth_header_size]
+    raw_data = msg.data[depth_header_size:]
+
+    depth_data_raw = cv2.imdecode(np.frombuffer(raw_data, np.uint8), cv2.IMREAD_UNCHANGED)
+    # rospy.loginfo(f"{depth_data_raw.shape} {depth_data_raw.dtype}")
+    if depth_data_raw is None:
+        raise Exception("Could not decode compressed depth image,"
+                        "probably wrong header size 'depth_header_size'!")
+
+    if depth_format == "16UC1":
+        raise Exception(f"Decoding of '{depth_format}' is not implemented!")
+    elif depth_format == "32FC1":
+        # header: int, float, float
+        [compresion_format, depth_quant_a, depth_quant_b] = struct.unpack('iff', raw_header)
+        depth_np = depth_quant_a / (depth_data_raw.astype(np.float32) - depth_quant_b)
+        # filter max values
+        depth_np[depth_data_raw == 0] = 0
+
+        text = f"{depth_quant_a} {depth_quant_b} - {np.max(depth_np)} {np.min(depth_np)}"
+        text += f" - {depth_np.shape} {depth_np.dtype}"
+        rospy.loginfo(text)
+        # depth_np provides distance in meters as f32
+        return depth_np
+    else:
+        raise Exception(f"Decoding of '{depth_format}' is not implemented!")
 
 
 def camera_to_intrinsic(camera_info: CameraInfo) -> o3d.camera.PinholeCameraIntrinsic:
@@ -71,18 +115,30 @@ class Open3DViz:
         self.roi_y = 100
         self.roi_h = 500
 
-        use_compressed_image = rospy.get_param("~compressed_image", True)
-
         camera_info_sub = message_filters.Subscriber("camera_info", CameraInfo)
-        # TODO(lucasw) support compressedDepth
-        depth_sub = message_filters.Subscriber("depth/image", Image)
-        # make this optional
-        if use_compressed_image:
+
+        use_compressed_color = rospy.get_param("~compressed_color", True)
+        use_compressed_depth = rospy.get_param("~compressed_depth", True)
+
+        if use_compressed_color:
             color_sub = message_filters.Subscriber("image/compressed", CompressedImage)
-            callback = self.compressed_callback
         else:
             color_sub = message_filters.Subscriber("image", Image)
+
+        if use_compressed_depth:
+            depth_sub = message_filters.Subscriber("depth/image/compressedDepth", CompressedImage)
+        else:
+            depth_sub = message_filters.Subscriber("depth/image", Image)
+
+        if use_compressed_color and use_compressed_depth:
+            callback = self.compressed_both_callback
+        elif use_compressed_color and not use_compressed_depth:
+            callback = self.compressed_color_callback
+        elif not use_compressed_color and use_compressed_depth:
+            callback = self.compressed_depth_callback
+        elif not use_compressed_color and not use_compressed_depth:
             callback = self.callback
+
         subs = [depth_sub, color_sub, camera_info_sub]
         # self._ets = message_filters.TimeSynchronizer(subs, queue_size=15)
         self.ets = message_filters.ApproximateTimeSynchronizer(subs, queue_size=15, slop=0.1)
@@ -149,25 +205,42 @@ class Open3DViz:
         self.viz.poll_events()
         self.viz.update_renderer()
 
-    def compressed_callback(self, depth_image: Image, compressed_color_image: CompressedImage,
-                            camera_info: CameraInfo):
+    # TODO(lucasw) make these importable also, put whole sync subscriber into re-usable class
+    def compressed_both_callback(self, depth_image: Image, compressed_color_image: CompressedImage,
+                                 camera_info: CameraInfo):
+        depth_np = decode_compressed_depth(depth_image)
         encoding = "bgr8"
         color_np = self.cv_bridge.compressed_imgmsg_to_cv2(compressed_color_image, encoding)
-        self.process(depth_image, color_np, camera_info)
+        self.process(depth_np, color_np, camera_info)
 
-    def callback(self, depth_image: Image, color_image: Image, camera_info: CameraInfo):
-        color_np = self.cv_bridge.imgmsg_to_cv2(depth_image, depth_image.encoding)
-        self.process(depth_image, color_np, camera_info)
+    def compressed_color_callback(self, depth_image: Image, compressed_color_image: CompressedImage,
+                                  camera_info: CameraInfo):
+        depth_np = self.cv_bridge.imgmsg_to_cv2(depth_image, depth_image.encoding)
+        encoding = "bgr8"
+        color_np = self.cv_bridge.compressed_imgmsg_to_cv2(compressed_color_image, encoding)
+        self.process(depth_np, color_np, camera_info)
 
-    def process(self, depth_image: Image, color_np: np.ndarray, camera_info: CameraInfo):
+    def compressed_depth_callback(self, depth_image: CompressedImage, color_image: Image,
+                                  camera_info: CameraInfo):
+        depth_np = decode_compressed_depth(depth_image)
+        encoding = "bgr8"
+        color_np = self.cv_bridge.imgmsg_to_cv2(color_image, encoding)
+        self.process(depth_np, color_np, camera_info)
+
+    def callback(self, depth_image: Image, color_image: Image,
+                 camera_info: CameraInfo):
+        depth_np = self.cv_bridge.imgmsg_to_cv2(depth_image, depth_image.encoding)
+        encoding = "bgr8"
+        color_np = self.cv_bridge.imgmsg_to_cv2(color_image, encoding)
+        self.process(depth_np, color_np, camera_info)
+
+    def process(self, depth_np: np.ndarray, color_np: np.ndarray, camera_info: CameraInfo):
         self.ets_used = True
         t0 = rospy.Time.now()
-        depth_np = self.cv_bridge.imgmsg_to_cv2(depth_image, depth_image.encoding)
-
         new_geometry = []
 
-        width = depth_image.width
-        height = depth_image.height
+        width = depth_np.shape[1]
+        height = depth_np.shape[0]
 
         x0 = self.roi_x
         y0 = self.roi_y
@@ -178,7 +251,7 @@ class Open3DViz:
             x1 = x0 + self.roi_w
 
         if self.roi_h is None:
-            y1 = depth_image.height
+            y1 = height
         else:
             y1 = y0 + self.roi_h
 
